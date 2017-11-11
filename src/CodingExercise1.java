2a5ed1916a907dddadef7ca07752cdaf6f160dbf
League <league@WTS22.attlocal.net>
2017-11-10 17:04:30 -0800

    exam

--- src/CodingExercise1.java
+++ src/CodingExercise1.java
@@ -1,19 +1,51 @@
 import java.awt.Color;
+
 import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 /* Level 0 Exam: Coding  Exercise #1 */
 public class CodingExercise1 {
 
-    public static void main(String[] args) {
-   	 // 3. ask the user what color they would like the Robot to draw
+	public static void main(String[] args) {
 
-   	 // 4. use an if/else statement to set the pen color that the user requested (minimum of 2 colors)
+		Robot Bob = new Robot();
 
-   	 // 2. set the pen width to 10
+		// 3. ask the user what color they would like the Robot to draw
+		String color = JOptionPane.showInputDialog("what color would you like the robot to draw?");
+		// 4. use an if/else statement to set the pen color that the user requested
+		// (minimum of 2 colors)
+		if (color.equals("blue")) {
+			Bob.penDown();
+			Bob.setPenColor(Color.blue);
+			Bob.setPenWidth(10);
+			Bob.penDown();
+			Bob.move(100);
+			Bob.turn(90);
+			Bob.move(100);
+			Bob.turn(90);
+			Bob.move(100);
+			Bob.turn(90);
+			Bob.move(100);
 
-   	 // 1. make the Robot draw a shape
-   	 
-    }
+		}
+		if (color.equals("green")) {
+			Bob.penDown();
+			Bob.setPenColor(Color.green);
+			Bob.setPenWidth(10);
+			Bob.penDown();
+			Bob.move(100);
+			Bob.turn(90);
+			Bob.move(100);
+			Bob.turn(90);
+			Bob.move(100);
+			Bob.turn(90);
+			Bob.move(100);
+			// 2. set the pen width to 10
 
+			// 1. make the Robot draw a shape
+
+		}
+
+	}
 }
